0c29f4823d62ff1b7b3f8156d69ae6a6f805d420
Eduardo Perez <edujpp1@gmail.com>
2019-07-16 07:47:07 -0300

    Se modificaron los componentes EventosContext y Formulario para realizar la consulta a la API y traer los eventos según la categoría que seleccione y las plabras claves que use el usuario

--- src/App.js
+++ src/App.js
@@ -2,15 +2,25 @@ import React, { Fragment } from 'react';
 import Header from './components/Header';
 import Formulario from './components/Formulario';
 import CategoriasProvider from './context/CategoriasContext';
+import EventosProvider from './context/EventosContext';
 
 function App() {
   return (
-    <CategoriasProvider>
-      <Header />
-      <div className="uk-container">
-        <Formulario />
-      </div>
-    </CategoriasProvider>
+    /*
+      El componente que se encargará de los eventos puede contener a todos
+      los demas componente, para que así, estos puedan utilizar sus métodos
+
+      También se colocar rodeando solo a los elementos que generarán un evento
+      pero no sabemos si más adelante los demás también lo necesitarán
+    */
+    <EventosProvider>
+      <CategoriasProvider>
+        <Header />
+        <div className="uk-container">
+          <Formulario />
+        </div>
+      </CategoriasProvider>
+    </EventosProvider>
   );
 }
 
--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-
 import { CategoriasConsumer } from '../context/CategoriasContext';
+import { EventosConsumer } from '../context/EventosContext';
 
 class Formulario extends Component {
   state = {
@@ -17,53 +17,65 @@ class Formulario extends Component {
 
   render(){
     return(
-      <form>
-        
-        <fieldset className="uk-fieldset uk-margin">
-          <legend className="uk-legend uk-text-center">
-            Busca tu evento por nombre o categoría
-          </legend>
-        </fieldset>
-        <div className="uk-column-1-3@m uk-margin">
-          <div className="uk-margin" uk-margin="true">
-            {/*
-              El name del input tiene que mapearse con los atributos
-              del state
-             */}
-            <input 
-              name="nombre"
-              className="uk-input"
-              type="text"
-              placeholder="Nombre de evento o ciudad"
-              onChange={this.obtenerDatosEvento}
-            />
-          </div>
-          <div className="uk-margin" uk-margin="true">
-            <select 
-              className="uk-select"
-              name="categoria"
-              onChange={this.obtenerDatosEvento}
-            >
-              <option value="">Selecciona una categoría</option>
-              <CategoriasConsumer>
-                {(value) => {
-                  return(
-                    value.categorias.map(categoria => (
-                      <option key={categoria.id} value={categoria.id} data-uk-form-select>
-                        {categoria.name_localized}
-                      </option>
-                    ))
-                  )
-                }}
-              </CategoriasConsumer>
-            </select>
-          </div>
-          <div className="uk-margin" uk-margin="true">
-             <input type="submit" className="uk-button uk-button-danger" value="Buscar eventos"/>
-          </div>
-        </div>
+      <EventosConsumer>
+        {(value) => {
 
-      </form>
+          return(
+            <form
+              onSubmit={e => {
+                e.preventDefault();
+                value.obtenerEventos(this.state);
+              }}
+            >
+              
+              <fieldset className="uk-fieldset uk-margin">
+                <legend className="uk-legend uk-text-center">
+                  Busca tu evento por nombre o categoría
+                </legend>
+              </fieldset>
+              <div className="uk-column-1-3@m uk-margin">
+                <div className="uk-margin" uk-margin="true">
+                  {/*
+                    El name del input tiene que mapearse con los atributos
+                    del state
+                  */}
+                  <input 
+                    name="nombre"
+                    className="uk-input"
+                    type="text"
+                    placeholder="Nombre de evento o ciudad"
+                    onChange={this.obtenerDatosEvento}
+                  />
+                </div>
+                <div className="uk-margin" uk-margin="true">
+                  <select 
+                    className="uk-select"
+                    name="categoria"
+                    onChange={this.obtenerDatosEvento}
+                  >
+                    <option value="">Selecciona una categoría</option>
+                    <CategoriasConsumer>
+                      {(value) => {
+                        return(
+                          value.categorias.map(categoria => (
+                            <option key={categoria.id} value={categoria.id} data-uk-form-select>
+                              {categoria.name_localized}
+                            </option>
+                          ))
+                        )
+                      }}
+                    </CategoriasConsumer>
+                  </select>
+                </div>
+                <div className="uk-margin" uk-margin="true">
+                  <input type="submit" className="uk-button uk-button-danger" value="Buscar eventos"/>
+                </div>
+              </div>
+
+            </form>
+          );
+        }}
+      </EventosConsumer>
     );
   }
 }
